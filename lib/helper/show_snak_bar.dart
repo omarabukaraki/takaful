8810530dc9d4f0b1ba5cab4e5ed72ac28b566499
omar <abukarakiomar@gmail.com>
2023-09-13 15:48:17 +0300

    create sank bar function

---
+++ lib/helper/show_snak_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void showSankBar(BuildContext context, String message,
+    {Color color = Colors.red}) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(message),
+    backgroundColor: color,
+  ));
+}
