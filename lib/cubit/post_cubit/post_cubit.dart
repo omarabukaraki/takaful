4c8f17abeee88064bd7df4afcfb5bd36d47d378b
omar <abukarakiomar@gmail.com>
2023-09-23 13:17:38 +0300

    add ignore

--- lib/cubit/post_cubit/post_cubit.dart
+++ lib/cubit/post_cubit/post_cubit.dart
@@ -1,6 +1,8 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 import 'package:takaful/models/post_model.dart';
 
