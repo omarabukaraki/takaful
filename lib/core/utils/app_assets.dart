324eb17c85cd20f50ad40cbbc8dfc61125ea414a
omar <abukarakiomar@gmail.com>
2023-09-24 17:36:25 +0300

    create a constant class for assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,25 @@
+abstract class Assets {
+  Assets._();
+  static const String assetsImageAdsPhotoOne = "assets/image/ads_photo_one.png";
+  static const String assetsImageAdsPhotoThree =
+      "assets/image/ads_photo_three.jpg";
+  static const String assetsImageAdsPhotoTwo = "assets/image/ads_photo_two.png";
+  static const String assetsImageAll = "assets/image/all.png";
+  static const String assetsImageBackgroundObjects =
+      "assets/image/Background objects.png";
+  static const String assetsImageClotherss = "assets/image/clotherss.png";
+  static const String assetsImageFoodlogin = "assets/image/foodlogin.png";
+  static const String assetsImageItemsImage = "assets/image/items_image.png";
+  static const String assetsImageK13 = "assets/image/K13.png";
+  static const String assetsImageMaskGroup2193 =
+      "assets/image/Mask Group 2193.png";
+  static const String assetsImagePizza = "assets/image/pizza.png";
+  static const String assetsImageServicesImage =
+      "assets/image/services_image.png";
+  static const String assetsImageSofa = "assets/image/sofa.png";
+  static const String assetsImageTeam01 = "assets/image/team-01.jpg";
+  static const String assetsImageThreeinone = "assets/image/threeinone.png";
+  static const String assetsImageUi = "assets/image/ui.png";
+  static const String assetsImageUntitled2 = "assets/image/Untitled-2.png";
+  static const String assetsImageUserImage = "assets/image/user_image.png";
+}
