3c89dd60b3fb45e51c2b6dddff620fd0e3bc9248
omar <abukarakiomar@gmail.com>
2023-09-24 17:38:56 +0300

    add color const class

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+abstract class AppColor {
+  static const Color kPrimary = Color(0xff3A44A0);
+  static const Color kFont = Color(0xff4A4B4D);
+  static const Color kTextFiled = Color(0xffF2F2F2);
+  static const Color kTextFiledFont = Color.fromARGB(255, 117, 117, 117);
+}
