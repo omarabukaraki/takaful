8b0c097493be2e623614c345fa0d7d9d191f8724
omar <abukarakiomar@gmail.com>
2023-12-12 00:36:13 +0300

    delete duplicate save donation cubit

--- lib/features/get_donation/presentation/cubit/save_donation_cubit/save_donation_model.dart
+++
@@ -1,9 +0,0 @@
-// class SaveDonationModel{
-//   String donationId ;
-//   String userId;
-//   SaveDonationModel({required this.donationId,required this.userId});
-//   factory SaveDonationModel.fromJson(jsonData){
-//     return SaveDonationModel(donationId: jsonData['donationId'], userId: jsonData['userId']);
-//   }
-
-// }
